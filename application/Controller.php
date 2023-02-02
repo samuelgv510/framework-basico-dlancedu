cc9fd2885595f96a6532426f9044a019a59cfe4c
Samuel Guerrero <samuelfrancisco.guerrerovilela@nttdata.com>
2023-02-02 17:59:09 -0500

    request y bootstrap

---
+++ application/Controller.php
@@ -0,0 +1,6 @@
+<?php
+
+abstract class Controller
+{
+    abstract public function index();
+}
--- index.php
+++ index.php
@@ -12,5 +12,14 @@ require_once APP_PATH . 'Model.php';
 require_once APP_PATH . 'View.php';
 require_once APP_PATH . 'Registro.php';
 
-echo '<pre>';
-print_r(get_required_files());
+//echo '<pre>';
+//print_r(get_required_files());
+// $r=new Request();
+// echo $r->getControlador().'<br>';
+// echo $r->getMetodo().'<br>';
+// print_r($r->getArgs());
+try {
+    Bootstrap::run(new Request);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
